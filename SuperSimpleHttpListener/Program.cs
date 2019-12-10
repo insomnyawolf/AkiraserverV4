c4534a0ec96a42e74c8606317cb0ca449d266fed
insomnyawolf <insomnyawolf@gmail.com>
2019-12-10 15:07:46 +0100

    Initial Project Files

---
+++ SuperSimpleHttpListener/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace SuperSimpleHttpListener
+{
+    internal static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var config = LoadConfiguration();
+
+            var serverConfig = config.GetSection("Server");
+
+            SuperSimpleHttpListener serv = new SuperSimpleHttpListener(serverConfig);
+            serv.StartListening();
+
+            await Task.Delay(-1);
+        }
+
+        private static IConfiguration LoadConfiguration()
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder();
+            builder = builder.AddJsonFile("config.json", optional: false, reloadOnChange: true);
+            return builder.Build();
+        }
+
+        private static ServiceProvider ConfigureServices()
+        {
+            ServiceCollection services = new ServiceCollection();
+            return services.BuildServiceProvider();
+        }
+    }
+}
---
+++ SuperSimpleHttpListener/SuperSimpleHttpListener.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace SuperSimpleHttpListener
+{
+    public class SuperSimpleHttpListener
+    {
+        public bool IsListening { get; private set; }
+        public HttpListener Listener { get; set; }
+
+        public SuperSimpleHttpListener(IConfigurationSection configuration)
+        {
+            if (!HttpListener.IsSupported)
+            {
+                throw new NotSupportedException("Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
+            }
+
+            // URI prefixes are required,
+            // for example "http://contoso.com:8080/index/".
+            if (configuration == null)
+            {
+                throw new ArgumentNullException(nameof(configuration));
+            }
+
+            // Create a listener.
+            Listener = new HttpListener();
+
+            // Add the prefixes.
+            foreach (KeyValuePair<string, string> s in configuration.GetSection("Adresses").AsEnumerable())
+            {
+                if (!string.IsNullOrWhiteSpace(s.Value))
+                {
+                    Listener.Prefixes.Add(s.Value);
+                }
+            }
+
+            // URI prefixes are required,
+            // for example "http://contoso.com:8080/index/".
+            if (Listener.Prefixes.Count == 0)
+            {
+                throw new ArgumentException($"No valid prefixes in {nameof(configuration)}");
+            }
+        }
+
+        public void StartListening()
+        {
+            Listener.Start();
+            IsListening = true;
+
+            while (IsListening)
+            {
+                // Note: The GetContext method blocks while waiting for a request.
+                HttpListenerContext context = Listener.GetContext();
+                HttpListenerRequest request = context.Request;
+                // Obtain a response object.
+                HttpListenerResponse response = context.Response;
+                // Construct a response.
+                string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                // Get a response stream and write the response to it.
+                response.ContentLength64 = buffer.Length;
+                System.IO.Stream output = response.OutputStream;
+                output.Write(buffer, 0, buffer.Length);
+                // You must close the output stream.
+                output.Close();
+            }
+
+            Listener.Stop();
+        }
+
+        public void StopListening()
+        {
+            IsListening = false;
+        }
+    }
+}
